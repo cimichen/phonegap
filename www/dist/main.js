186ee4f8964a462c64b97f8361c608e05d3e99cc
cimichen <cimi5210@163.com>
2016-05-16 09:10:40 +0800

    添加视频

---
+++ www/dist/main.js
@@ -0,0 +1,24 @@
+/**
+ * Created by yangl on 2016/5/15.
+ */
+
+function getParams(key) {
+    var keyList = [];
+    var valueList = [];
+    var hrefStr = decodeURIComponent(window.location.href);
+    var strArray = hrefStr.split("?");
+    var paramStr = strArray[1];
+    if ((paramStr != null) && (paramStr != null)) {
+        var paramArray = paramStr.split("&");
+        for (var i = 0; i < paramArray.length; i++) {
+            var param = paramArray[i];
+            valueList.push(param.substr(param.indexOf("=") + 1));
+            keyList.push(param.substr(0, param.indexOf("=")));
+        }
+        for (var j = 0; j < keyList.length; j++) {
+            if (key == keyList[j]) {
+                return valueList[j];
+            }
+        }
+    }
+}
